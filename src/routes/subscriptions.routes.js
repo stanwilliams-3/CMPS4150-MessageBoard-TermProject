4619ab7d87aaee556806f8fab7e27d0f01e9dfd7
Stanley Williams <stanley.williams-3@selu.edu>
2026-04-21 16:23:16 -0500

    feat: subscription routes.

--- src/routes/subscriptions.routes.js
+++ src/routes/subscriptions.routes.js
@@ -1,1 +1,8 @@
-// MVC placeholder
+import { Router } from "express";
+import { subscriptionController } from "../controllers/subscriptionController.js";
+
+const router = Router();
+
+router.post("/subscribe", subscriptionController.subscribe);
+
+export default router;
